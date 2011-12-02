20f57e59a9a95de242e8f3e4de84a9c9d03025e2
snake <bpchaoxin@163.com>
2011-12-02 17:44:47 +0800

    Core Data Persistence

--- Core Data Persistence/MainViewController.h
+++ Core Data Persistence/MainViewController.h
@@ -11,7 +11,17 @@
 #import <CoreData/CoreData.h>
 
 @interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+{
+    UITextView *line1;
+    UITextView *line2;
+    UITextView *line3;
+    UITextView *line4;
+}
 
 @property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (retain, nonatomic) IBOutlet UITextField *line1;
+@property (retain, nonatomic) IBOutlet UITextField *line2;
+@property (retain, nonatomic) IBOutlet UITextField *line3;
+@property (retain, nonatomic) IBOutlet UITextField *line4;
 
 @end
--- Core Data Persistence/MainViewController.m
+++ Core Data Persistence/MainViewController.m
@@ -7,10 +7,15 @@
 //
 
 #import "MainViewController.h"
+#import "AppDelegate.h"
 
 @implementation MainViewController
 
 @synthesize managedObjectContext = _managedObjectContext;
+@synthesize line1 = _line1;
+@synthesize line2 = _line2;
+@synthesize line3 = _line3;
+@synthesize line4 = _line4;
 
 - (void)didReceiveMemoryWarning
 {
@@ -24,10 +29,82 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    AppDelegate *appDeleget = [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *context = [appDeleget managedObjectContext];
+    NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Line" inManagedObjectContext:context];
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    [request setEntity:entityDescription];
+    
+    NSError *error;
+    NSArray *objects = [context executeFetchRequest:request error:&error];
+    if (nil ==  objects) {
+        NSLog(@"There was an error!");
+    }
+    
+    for(NSManagedObject *oneObject in objects)
+    {
+        NSNumber *lineNum = [oneObject valueForKey:@"lineNum"];
+        NSString *lineText = [oneObject valueForKey:@"lineText"];
+        
+        NSString *fieldName = [NSString stringWithFormat:@"line%d", [lineNum integerValue]];
+        UITextField *theField = [self valueForKey:fieldName];
+        theField.text = lineText;
+    }
+    [request release];
+    
+    UIApplication *app = [UIApplication sharedApplication];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationWillResignActive:)
+                                                 name:UIApplicationWillResignActiveNotification
+                                               object:app];
+}
+
+- (void)applicationWillResignActive:(NSNotification *)notification
+{
+    AppDelegate *appDelegate = [[UIApplication sharedApplication]delegate];
+    NSManagedObjectContext *context = [appDelegate managedObjectContext];
+    
+    NSError *error;
+    for (int i = 1; i <= 4; i++) {
+        NSString *fieldName = [NSString stringWithFormat:@"line%d", i];
+        UITextField *theField = [self valueForKey:fieldName];
+        NSFetchRequest *request = [[NSFetchRequest alloc] init];
+        
+        NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Line" inManagedObjectContext:context];
+        
+        [request setEntity:entityDescription];
+        
+        NSPredicate *pred = [NSPredicate predicateWithFormat:@"(lineNum = %d)", i];
+        [request setPredicate:pred];
+        
+        NSManagedObject *theLine = nil;
+        
+        NSArray *objects = [context executeFetchRequest:request error:&error];
+        
+        if (nil == objects) {
+            NSLog(@"There was an error!");
+        }
+        if ([objects count] > 0) {
+            theLine = [objects objectAtIndex:0];
+        }
+        else
+        {
+            theLine = [NSEntityDescription insertNewObjectForEntityForName:@"Line" inManagedObjectContext:context];
+        }
+        [theLine setValue:[NSNumber numberWithInt:i] forKey:@"lineNum"];
+        [theLine setValue:theField.text forKey:@"lineText"];
+        
+        [request release];
+    }
+    [context save:&error];
 }
 
 - (void)viewDidUnload
 {
+    [self setLine1:nil];
+    [self setLine2:nil];
+    [self setLine3:nil];
+    [self setLine4:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -76,6 +153,10 @@
 - (void)dealloc
 {
     [_managedObjectContext release];
+    [_line1 release];
+    [_line2 release];
+    [_line3 release];
+    [_line4 release];
     [super dealloc];
 }
 
